e35eadc49787c426e312a8423c077422419a51fe
bjan816 <bjan816@aucklanduni.ac.nz>
2023-11-27 22:41:39 +0900

    Implement Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { ComputersCanvas } from './canvas'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915eff]' />
+          <div className='w-1 h-40 sm:h-80 violet-gradient' />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I&apos;m&nbsp;
+            <span className='text-[#915eff]'>
+              Borim
+            </span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I am in my final year of BSc Computer Science at The University of Auckland, New Zealand.&nbsp;
+            <br className='hidden sm:block' />
+            Currently, my interests lie in software development and security, with some data science on the side.&nbsp;
+            <br className='hidden sm:block' />
+            I'm committed to learning as much as I can and developing high-quality, functional software.
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+    </section>
   )
 }
 
