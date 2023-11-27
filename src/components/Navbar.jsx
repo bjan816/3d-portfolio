be885ead30e86d9df21d6fdaf6a8f1a9f31a2c4c
bjan816 <bjan816@aucklanduni.ac.nz>
2023-11-27 17:59:05 +0900

    Implement Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,9 +20,20 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Borim <span className='sm:block hidden'>| Jang</span></p>
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">Borim <span className="sm:block hidden">| Jang</span></p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${active === link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
 
