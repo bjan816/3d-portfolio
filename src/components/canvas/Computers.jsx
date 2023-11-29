eb79d2eb78d1ae59317444ae4cb2e2d4c1cacda5
bjan816 <bjan816@aucklanduni.ac.nz>
2023-11-29 17:00:20 +0900

    Install react-spline

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,6 +2,8 @@ import React, { Suspense, useEffect, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
+import Spline from '@splinetool/react-spline';
+
 
 const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
